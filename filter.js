1e39b2c27c54a09e74d8e5d98cd438b43f4ccb42
KamilGaribov <kamilgarib@gmail.com>
2019-11-10 04:02:15 +0400

    09nov2019_

--- filter.js
+++ filter.js
@@ -1,3 +1,77 @@
+class stadion {
+    constructor(name, city, district, metro, adress, price, floor, cover, size, bath, cafe, video, park, img1, img2, img3) {
+        this.name = name;
+        this.city = city;
+        this.district = district;
+        this.metro = metro;
+        this.adress = adress;
+        this.price = price;
+        this.floor = floor;
+        this.cover = cover;
+        this.size = size;
+        this.bath = bath;
+        this.cafe = cafe;
+        this.video = video;
+        this.park = park;
+        this.img1 = img1;
+        this.img2 = img2;
+        this.img3 = img3;
+    }
+}
+
+var Ruslan93_asagi = new stadion("Ruslan 93 (böyük)", "Bakı", "Nizami", "Q.Qarayev", "Əli Vəliyev 7a", 50, "Ot xalça", false, "6vs6", true, false, false, 'stadium_images/Ruslan93_asagi/1.jpg', 'stadium_images/Ruslan93_asagi/2.jpg', 'stadium_images/Ruslan93_asagi/3.jpg');
+var Ruslan93_yuxari = new stadion("Ruslan 93 (kiçik)", "Bakı", "Nizami", "Q.Qarayev", "Əli Vəliyev 7a", 40, "Ot xalça", false, "5vs5", true, false, false, 'stadium_images/Ruslan93_yuxari/1.jpeg', 'stadium_images/Ruslan93_yuxari/2.jpg', 'stadium_images/Ruslan93_yuxari/3.jpg');
+var Serhedci_boyuk = new stadion("Sərhədçi (böyük)", "Bakı", "Xətai", "Xalqlar D.", "Babək pr.23/16", 100, "Ot xalça", false, "11vs11", false, false, true, 'stadium_images/Serhedci_boyuk/1.jpg', 'stadium_images/Serhedci_boyuk/2.jpg', 'stadium_images/Ruslan93_yuxari/3.jpg');
+var Serhedci_xalca = new stadion("Sərhədçi (xalça)", "Bakı", "Xətai", "Xalqlar D.", "Babək pr.23/16", 60, "Ot xalça", true, "6vs6", false, false, true, 'stadium_images/Ruslan93_asagi/1.jpg', 'stadium_images/Ruslan93_asagi/2.jpg', 'stadium_images/Ruslan93_asagi/2.jpg');
+var Serhedci_futzal = new stadion("Sərhədçi (futzal)", "Bakı", "Xətai", "Xalqlar D.", "Babək pr.23/16", 60, "Rezin", true, "6vs6", false, false, true, 'stadium_images/Serhedci_futzal/1.jpeg', 'stadium_images/Serhedci_futzal/2.jpeg', 'stadium_images/Serhedci_futzal/3.jpeg');
+var FLand = new stadion("F-Land", "Bakı", "Nizami", "Neftçilər", "Çobanzadə 2404", 30, "Ot xalça", false, "5vs5", false, false, true, 'stadium_images/FLand/1.jpeg', 'stadium_images/FLand/2.jpeg', 'stadium_images/FLand/3.jpeg');
+var Tenis_Ak = new stadion("Tenis Akademiyası", "Bakı", "Binəqədi", "Azaqlıq", "İbrahimpaşa Dadaşov 157", 60, "Ot xalça", true, "6vs6", false, false, true, 'stadium_images/Tenis_Ak/1.jpeg', 'stadium_images/Tenis_Ak/2.jpeg', 'stadium_images/Tenis_Ak/3.jpeg');
+var BOS = new stadion("Bakı Olimpiya Stadionu", "Bakı", "Sabunçu", "Koroğlu", "H.Əliyev pr.323", 60, "Ot xalça", true, "6vs6", true, false, true, 'stadium_images/BOS/1.jpeg', 'stadium_images/BOS/2.jpeg', 'stadium_images/BOS/3.jpeg');
+var stadion_707 = new stadion("707", "Bakı", "Binəqədi", "Azadlıq", "Şövkət Məmmədova 2", 35, "Ot xalça", true, "6vs6", true, true, true, 'stadium_images/stadion_707/1.jpeg', 'stadium_images/stadion_707/2.jpeg', 'stadium_images/stadion_707/3.jpeg');
+
+
+// Binəqədi rayonu
+var azfar_248 = new stadion("AzFar 248 saylı Stadion", "Bakı", "Binəqədi", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_248/1.jpeg', 'stadium_images/azfar_248/2.jpeg', 'stadium_images/azfar_248/3.jpg');
+var azfar_267 = new stadion("AzFar 267 saylı Stadion", "Bakı", "Binəqədi", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_267/1.jpg', 'stadium_images/azfar_267/2.jpeg', 'stadium_images/azfar_267/3.jpeg');
+var azfar_115 = new stadion("AzFar 115 saylı Stadion", "Bakı", "Binəqədi", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_115/1.jpeg', 'stadium_images/azfar_115/2.jpeg', 'stadium_images/azfar_115/3.jpeg');
+var azfar_bineqediGencler = new stadion("AzFar Binəqədi Gənclər Evi Stadionu", "Bakı", "Binəqədi", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_bineqediGencler/1.jpeg', 'stadium_images/azfar_bineqediGencler/2.jpeg', 'stadium_images/azfar_bineqediGencler/3.jpeg');
+
+// Nərimanov rayonu
+var azfar_47 = new stadion("AzFar 47 saylı Stadion", "Bakı", "Nərimanov", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_47/1.jpeg', 'stadium_images/azfar_47/2.jpeg', 'stadium_images/azfar_47/3.jpeg');
+var azfar_200 = new stadion("AzFar 200 saylı Stadion", "Bakı", "Nərimanov", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_200/1.jpeg', 'stadium_images/azfar_200/2.jpeg', 'stadium_images/azfar_200/3.jpeg');
+
+// Nəsimi rayonu
+var azfar_20Yanvar = new stadion("AzFar 20 Yanvar Stadionu", "Bakı", "Nəsimi", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_20Yanvar/1.jpeg', 'stadium_images/azfar_20Yanvar/2.jpeg', 'stadium_images/azfar_20Yanvar/3.jpeg');
+
+// Nizami rayonu
+var azfar_shefa = new stadion("AzFar Şəfa Stadionu", "Bakı", "Nizami", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_shefa/1.jpeg', 'stadium_images/azfar_shefa/2.jpeg', 'stadium_images/azfar_shefa/3.jpeg');
+var azfar_bakcell = new stadion("AzFar Bakcell Stadionu", "Bakı", "Nizami", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_bakcell/1.jpeg', 'stadium_images/azfar_bakcell/2.jpeg', 'stadium_images/azfar_bakcell/3.jpeg');
+
+// Xətai rayonu
+var azfar_264 = new stadion("AzFar 264 saylı Stadion", "Bakı", "Xətai", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_264/1.jpeg', 'stadium_images/azfar_264/2.jpeg', 'stadium_images/azfar_264/3.jpeg');
+var azfar_171 = new stadion("AzFar 171 saylı Stadion", "Bakı", "Xətai", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_171/1.jpeg', 'stadium_images/azfar_171/2.jpeg', 'stadium_images/azfar_171/3.jpeg');
+
+// Xəzər rayonu
+var azfar_124 = new stadion("AzFar 124 saylı Stadion", "Bakı", "Xəzər", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_124/1.jpeg', 'stadium_images/azfar_124/2.jpeg', 'stadium_images/azfar_124/3.jpeg');
+
+// Sabunçu rayonu
+var azfar_74 = new stadion("AzFar 74 saylı Stadion", "Bakı", "Sabunçu", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_74/1.jpeg', 'stadium_images/azfar_74/2.jpeg', 'stadium_images/azfar_74/3.jpeg');
+
+// Yasamal rayonu
+var azfar_BDU = new stadion("AzFar Bakı Dövlət Universiteti", "Bakı", "Yasamal", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_BDU/1.jpeg', 'stadium_images/azfar_BDU/2.jpeg', 'stadium_images/azfar_BDU/3.jpeg');
+var azfar_53 = new stadion("AzFar 53 saylı Stadion", "Bakı", "Yasamal", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_53/1.jpeg', 'stadium_images/azfar_53/2.jpeg', 'stadium_images/azfar_53/3.jpeg');
+var azfar_21 = new stadion("AzFar 21 saylı Stadion", "Bakı", "Yasamal", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_21/1.jpeg', 'stadium_images/azfar_21/2.jpeg', 'stadium_images/azfar_21/3.jpeg');
+
+// Sumqayıt
+var azfar_sumqayit = new stadion("AzFar Sumqayıt Stadionu", "Sumqayıt", "Sumqayıt Bulvar", "Azadlıq", "İbrahimpaşa Dadaşov", 70, "Ot xalça", true, "6vs6", true, true, true, true, 'stadium_images/azfar_sumqayit/1.jpeg', 'stadium_images/azfar_sumqayit/2.jpeg', 'stadium_images/azfar_sumqayit/3.jpeg');
+
+// Stadionlarin oldugu array
+var stadions = [Ruslan93_asagi, azfar_248, azfar_267, BOS, azfar_115, stadion_707, azfar_bineqediGencler, Ruslan93_yuxari, azfar_47, azfar_200, Serhedci_boyuk, azfar_20Yanvar, Serhedci_xalca, azfar_shefa, azfar_bakcell, Serhedci_futzal, azfar_264, azfar_171, FLand, azfar_124, azfar_74, Tenis_Ak, azfar_BDU, azfar_53, azfar_21, azfar_sumqayit]
+
+//////////
+
+
+
 var cityselect = document.createElement("select")
 var city1 = document.createElement("option")
 city1.innerHTML = "Şəhər"
@@ -103,6 +177,7 @@ parkradio.type = "checkbox"
 parkselect.append(parkradio)
 document.getElementById("radios").append(parkselect)
 
+var secondData = otherProject.database().ref();
 
 function axtaris(){
     var cityval = document.querySelector("#options select:nth-child(1)").value
@@ -180,11 +255,123 @@ function axtaris(){
         count = 0
     }
     console.log(finalarr)
-    var secondData = otherProject.database().ref();
     secondData.set({
         sentdata: finalarr
     })
+
 };
+
+
+//firebase 2
+var dataVal;
+var sentVal;
+var sentKey;
+var qlavniArr = []
+secondData.on('value', function (callback) {
+    dataVal = callback.val();
+    sentKey = Object.keys(dataVal);
+    sentVal = Object.values(dataVal);
+    // console.log(sentVal[Object.keys(sentVal)])
+    qlavniArr = sentVal[Object.keys(sentVal)]
+    // console.log(qlavniArr)
+    if(qlavniArr.length != 0){
+        document.getElementById("cardsarea").innerHTML = ""
+
+        for(let j=0; j<qlavniArr.length; j++){
+            for(let i=0; i<stadions.length; i++){
+                if(qlavniArr[j] == stadions[i].name){
+                    console.log(stadions[i].name)
+                    var example1_carddiv = document.createElement("div")
+                    example1_carddiv.setAttribute("class", "example-1 cardsarea col-sm-12 col-md-6 col-lg-4 col-xl-4")
+                
+                    var wrapperdiv = document.createElement("div")
+                    wrapperdiv.setAttribute("class", "wrapper")
+                
+                    var datadiv = document.createElement("div")
+                    datadiv.setAttribute("class", "data")
+                
+                    var contentdiv = document.createElement("div")
+                    contentdiv.setAttribute("class", "content")
+                
+                    var namespan = document.createElement("h2")
+                    namespan.setAttribute("class", "name")
+                    namespan.innerHTML = stadions[i].name + '<br>';
+                
+                    var infoh1 = document.createElement("h3")
+                    infoh1.setAttribute("class", "info")
+                    infoh1.innerHTML = `Qiymət: ${stadions[i].price} AZN` + '<br>' + `Ölçü: ${stadions[i].size}`;
+                
+                    var textdiv = document.createElement("p")
+                    textdiv.setAttribute("class", "text")
+                    textdiv.innerHTML = stadions[i].city + " şəhəri, " + stadions[i].district + " r., " + stadions[i].metro + " metrosu yaxınlığında yerləşən,  yüksək standartlara cavab verən " + stadions[i].name + " idman kompleksi. Burada siz öz asudə vaxtınızı ..."
+    
+                    var newlink = document.createElement("a")
+                    newlink.setAttribute("href", "nsayli.html")
+    
+                    example1_carddiv.append(wrapperdiv)
+                    wrapperdiv.append(datadiv)
+    
+                    newlink.append(contentdiv)
+                    
+                    contentdiv.append(namespan)
+                    contentdiv.append(infoh1)
+                    contentdiv.append(textdiv)
+                    datadiv.append(newlink)
+    
+                    document.getElementById("cardsarea").append(example1_carddiv)
+                }
+            }
+        }
+    }
+ });
+
+
+
+// cardlarin yaradilib atilmasi
+for(let i=0; i<stadions.length; i++){
+    var example1_carddiv = document.createElement("div")
+    example1_carddiv.setAttribute("class", "example-1 cardsarea col-sm-12 col-md-6 col-lg-4 col-xl-4")
+
+    var wrapperdiv = document.createElement("div")
+    wrapperdiv.setAttribute("class", "wrapper")
+
+    var datadiv = document.createElement("div")
+    datadiv.setAttribute("class", "data")
+
+    var contentdiv = document.createElement("div")
+    contentdiv.setAttribute("class", "content")
+
+    var namespan = document.createElement("h2")
+    namespan.setAttribute("class", "name")
+    namespan.innerHTML = stadions[i].name + '<br>';
+
+    var infoh1 = document.createElement("h3")
+    infoh1.setAttribute("class", "info")
+    infoh1.innerHTML = `Qiymət: ${stadions[i].price} AZN` + '<br>' + `Ölçü: ${stadions[i].size}`;
+
+    var textdiv = document.createElement("p")
+    textdiv.setAttribute("class", "text")
+    textdiv.innerHTML = stadions[i].city + " şəhəri, " + stadions[i].district + " r., " + stadions[i].metro + " metrosu yaxınlığında yerləşən,  yüksək standartlara cavab verən " + stadions[i].name + " idman kompleksi. Burada siz öz asudə vaxtınızı ..."
+
+    
+    var newlink = document.createElement("a")
+    newlink.setAttribute("href", "nsayli.html")
+
+    example1_carddiv.append(wrapperdiv)
+    wrapperdiv.append(datadiv)
+
+    newlink.append(contentdiv)
+
+    contentdiv.append(namespan)
+    contentdiv.append(infoh1)
+    contentdiv.append(textdiv)
+    datadiv.append(newlink)
+    document.getElementById("cardsarea").append(example1_carddiv);``
+    // console.log(stadions[i].img1)
+    document.querySelectorAll('.wrapper')[i].style.backgroundImage = `url(${stadions[i].img1})`;
+
+} ;
+
 
 
 
